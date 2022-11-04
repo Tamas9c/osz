e041631dc2db8349c00882de88a466d762da502a
Tamas9c <55052371+Tamas9c@users.noreply.github.com>
2022-11-04 10:34:07 +0100

    Add files via upload

---
+++ Új mappa/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.navbar-nav li').click(function (x) {
+      $('.navbar-nav li').removeClass('active');
+      $(event.target).parent().addClass('active');
+    });
+  });
